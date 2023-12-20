289ab60a5472a9f52fe7483f4e52e15183c43364
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2023-12-19 23:25:57 -0300

    add: calculo y mejora diseño

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
-const getCalcularMoneda = async () => {
-    const ApiRouter = "https://mindicador.cl/api";
-    const response = await fetch(ApiRouter);
-    const DataConversorMoneda = await response.json();
-   
-  
-    console.log(DataConversorMoneda);
-  };
-  getCalcularMoneda();
-  
+const selectMonedaOrigen = document.getElementById("moneda_origen");
+const btnCalcular = document.getElementById("botonCalcular");
+const inputCantidadUno = document.getElementById("cantidad_uno");
+const inputResultadoDivisa = document.getElementById("resultado");
 
+const obtenerTasaDeCambio = async () => {
+  const monedaOrigen = selectMonedaOrigen.value;
+
+  const ApiRouter = `https://mindicador.cl/api/${monedaOrigen}`;
+  const response = await fetch(ApiRouter);
+  const dataConversorMoneda = await response.json();
+  return dataConversorMoneda.serie[0].valor;
+};
+
+const calcular = async () => {
+  const monedaOrigen = selectMonedaOrigen.value;
+
+  const cantidadUno = parseFloat(inputCantidadUno.value);
+  const tasaDeCambio = await obtenerTasaDeCambio(monedaOrigen);
+  const resultado = cantidadUno * tasaDeCambio;
+
+  inputResultadoDivisa.value = resultado.toFixed(2);
+
+  console.log(inputResultadoDivisa.value);
+};
+
+btnCalcular.addEventListener("click", calcular);
