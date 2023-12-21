1e5b7da2ca8660ef53661c7e7df8069969c75e92
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2023-12-20 22:38:01 -0300

    dd: codigo mas legible t renderizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,59 @@
 const selectMonedaOrigen = document.getElementById("moneda_origen");
+
 const btnCalcular = document.getElementById("botonCalcular");
 const inputCantidadUno = document.getElementById("cantidad_uno");
 const inputResultadoDivisa = document.getElementById("resultado");
+const api_url = "https://mindicador.cl/api/";
 
-const obtenerTasaDeCambio = async () => {
-  const monedaOrigen = selectMonedaOrigen.value;
+async function getTipoMoneda(url) {
+  try {
+    const monedas = await fetch(url);
+    const { dolar, ivp, euro, uf, utm } = await monedas.json();
+    return [dolar, ivp, euro, uf, utm];
+  } catch (error) {
+    throw new Error(error);
+  }
+}
 
-  const ApiRouter = `https://mindicador.cl/api/${monedaOrigen}`;
-  const response = await fetch(ApiRouter);
-  const dataConversorMoneda = await response.json();
-  return dataConversorMoneda.serie[0].valor;
-};
+async function rederizaInfoMoneda(url) {
+  try {
+    const select_container = document.getElementById("moneda_origen");
+    const tipoMoneda = await getTipoMoneda(url);
 
-const calcular = async () => {
+    tipoMoneda.forEach((info_moneda) => {
+      const option = document.createElement("option");
+      option.value = info_moneda["codigo"];
+      option.innerText = info_moneda["nombre"];
+
+      select_container.appendChild(option);
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+async function getMonedaId(url, IdTipoMoneda) {
+  try {
+    if (IdTipoMoneda) {
+      const coin = await fetch(`${url}${IdTipoMoneda}`);
+      const { serie } = await coin.json();
+      const [{ valor: valorMoneda }] = serie;
+
+      return valorMoneda;
+    } else {
+      alert("Seleciona tipo moneda");
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+async function calcular() {
   const monedaOrigen = selectMonedaOrigen.value;
   const cantidadUno = parseFloat(inputCantidadUno.value);
-  const tasaDeCambio = await obtenerTasaDeCambio(monedaOrigen);
-  const resultado = cantidadUno * tasaDeCambio;
+  const calcularMoneda = await getMonedaId(api_url, monedaOrigen);
+  const resultado = cantidadUno / parseFloat(calcularMoneda);
 
   inputResultadoDivisa.innerHTML = resultado.toFixed(2);
-};
-
+}
 btnCalcular.addEventListener("click", calcular);
+rederizaInfoMoneda(api_url);
